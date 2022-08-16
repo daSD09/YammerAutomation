93af589774742bd426ebaaeb651163561832b4fa
daSD09 <89569856+daSD09@users.noreply.github.com>
2022-08-17 03:15:33 +0530

    Update Util.java

--- testing/src/main/java/com/yammer/utility/Util.java
+++ testing/src/main/java/com/yammer/utility/Util.java
@@ -3,6 +3,6 @@ package com.yammer.utility;
 public class Util {
 	
 	
-	public   static final  String EMAIL= "shramana.das@capgemini.com";
+	public   static final  String EMAIL= "abc@capgemini.com";
 
 }
